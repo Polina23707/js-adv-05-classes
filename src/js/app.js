3cf2d4d2c6735f8086cef6b12c1b660b4135749b
Polina Gorelik <Polina23707@gmail.com>
2023-11-18 14:12:15 +0200

    app.js and app.test.js done

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,66 @@
 // TODO: write your code here
-import sum from './basic';
 
-console.log('worked');
+export class Character {
+  constructor(name , type) {
+    this.name = name;
+    this.type = type;
+    this.health = 100;
+    this.level = 1;
 
-console.log(sum([1, 2]));
+    if (!((typeof name) === 'string' && name.length >= 2 && name.length <= 10)) {
+      throw new Error('Имя персонажа должно быть строкой от 2 до 10 символов!');
+    }
+
+    if (!['Bowerman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'].includes(type)) {
+      throw new Error('Тип персонажа задан не верно!');
+    }
+  }
+}
+
+export class Bowerman extends Character {
+  constructor(name, type) {
+    super(name, type);
+    this.attack = 25;
+    this.defence = 25;
+  }
+}
+
+export class Swordsman extends Character {
+  constructor(name, type) {
+    super(name, type);
+    this.attack = 40;
+    this.defence = 10;
+  }
+}
+
+export class Magician extends Character {
+  constructor(name, type) {
+    super(name, type);
+    this.attack = 40;
+    this.defence = 40;
+  }
+}
+
+export class Daemon extends Character {
+  constructor(name, type) {
+    super(name, type);
+    this.attack = 10;
+    this.defence = 40;
+  }
+}
+
+export class Undead extends Character {
+  constructor(name, type) {
+    super(name, type);
+    this.attack = 25;
+    this.defence = 25;
+  }
+}
+
+export class Zombie extends Character {
+  constructor(name, type) {
+    super(name, type);
+    this.attack = 40;
+    this.defence = 10;
+  }
+}
