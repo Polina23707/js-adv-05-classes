81b2891a2d9604618189add3fbfe51ddb5bf2b74
Polina Gorelik <Polina23707@gmail.com>
2023-11-24 12:22:18 +0200

    split app.js to separate classes / added import

--- src/js/app.js
+++ src/js/app.js
@@ -1,91 +1,18 @@
 // TODO: write your code here
 
-export class Character {
-  constructor(name , type) {
-    this.name = name;
-    this.type = type;
-    this.health = 100;
-    this.level = 1;
+import Character from "./class/Character"; 
+import Bowerman from "./class/Bowerman"; 
+import Daemon from "./class/Daemon"; 
+import Magician from "./class/Magician"; 
+import Swordsman from "./class/Swordsman"; 
+import Undead from "./class/Undead"; 
+import Zombie from "./class/Zombie";
 
-    if (!((typeof name) === 'string' && name.length >= 2 && name.length <= 10)) {
-      throw new Error('Имя персонажа должно быть строкой от 2 до 10 символов!');
-    }
-
-    if (!['Bowerman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'].includes(type)) {
-      throw new Error('Тип персонажа задан не верно!');
-    }
-  }
-
-  levelUp() {
-    if (this.health !== 0) {
-      this.level = this.level + 1;
-      this.attack = Math.round(this.attack * 1.2);
-      this.defence = Math.round(this.defence * 1.2);
-      this.health = 100;
-    } else {
-      throw new Error('Невозможно повысить уровень, персонаж умер!');
-    }   
-  }
-
-  damage(points) {
-    if (this.health >= 0) {
-      this.health = this.health - Math.round(points * (1 - this.defence / 100));
-    }
-  }
-}
-
-export class Bowerman extends Character {
-  constructor(name, type) {
-    super(name, type);
-    this.attack = 25;
-    this.defence = 25;
-  }
-}
-
-export class Swordsman extends Character {
-  constructor(name, type) {
-    super(name, type);
-    this.attack = 40;
-    this.defence = 10;
-  }
-}
-
-export class Magician extends Character {
-  constructor(name, type) {
-    super(name, type);
-    this.attack = 40;
-    this.defence = 40;
-  }
-}
-
-export class Daemon extends Character {
-  constructor(name, type) {
-    super(name, type);
-    this.attack = 10;
-    this.defence = 40;
-  }
-}
-
-export class Undead extends Character {
-  constructor(name, type) {
-    super(name, type);
-    this.attack = 25;
-    this.defence = 25;
-  }
-}
-
-export class Zombie extends Character {
-  constructor(name, type) {
-    super(name, type);
-    this.attack = 40;
-    this.defence = 10;
-  }
-}
-
-// const zombie1 = new Zombie('Zombie', 'Zombie');
-// console.log(zombie1.health);
-// zombie1.damage(20);
-// console.log(zombie1.health);
-// zombie1.health = 0;
-// console.log(zombie1.health);
-// zombie1.levelUp();
+let character = new Character('character', 'Bowman');
+let bowerman = new Bowerman('bowerman');
+let daemon = new Daemon('daemon');
+let magician = new Magician('magician');
+let swordsman = new Swordsman('swordsman');
+let undead = new Undead('undead');
+let zombie = new Zombie('zombie');
+console.log(character, bowerman, daemon, magician, swordsman, undead, zombie);
